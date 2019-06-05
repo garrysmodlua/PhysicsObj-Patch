426c1d394c95bb60a8ee3d6b46f7a19e195dfbba
CaptainPRICE <captainprice@programmer.net>
2019-06-05 08:25:59 +0200

    Update sv_buoyancyratio_patch.lua

--- lua/autorun/server/sv_buoyancyratio_patch.lua
+++ lua/autorun/server/sv_buoyancyratio_patch.lua
@@ -1,16 +1,21 @@
 local PhysObj = FindMetaTable 'PhysObj'
-if not PhysObj then return end
+if not PhysObj or PhysObj.GetBuoyancyRatio then return end
 
-local IsValid = IsValid
+local IsValid, isnumber = IsValid, isnumber
 
-function PhysObj.GetBuoyancyRatio(this)
-    return IsValid(this) and this.m_fBuoyancyRatio or 0
+do
+    local DefaultRatio = 0.0 -- TODO: This value should be parsed/read from a model instead of being hardcoded.
+    function PhysObj.GetBuoyancyRatio(this)
+        return IsValid(this) and isnumber(this.m_fBuoyancyRatio) and this.m_fBuoyancyRatio or DefaultRatio
+    end
 end
 
-local isnumber, max, min, PhysObj_SetBuoyancyRatio = isnumber, math.max, math.min, PhysObj.SetBuoyancyRatio
-function PhysObj.SetBuoyancyRatio(this, ratio)
-    if IsValid(this) and isnumber(ratio) then
-        this.m_fBuoyancyRatio = min(max(ratio, 0), 1)
-        PhysObj_SetBuoyancyRatio(this, ratio)
+do
+    local max, min, PhysObj_SetBuoyancyRatio = math.max, math.min, PhysObj.SetBuoyancyRatio
+    function PhysObj.SetBuoyancyRatio(this, ratio)
+        if IsValid(this) and isnumber(ratio) and ratio == ratio then
+            this.m_fBuoyancyRatio = min(max(ratio, 0.0), 1.0)
+            return PhysObj_SetBuoyancyRatio(this, ratio)
+        end
     end
 end
